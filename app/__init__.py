b385b61742e69467c8f556288ea120ec8b3f46d8
Danielle Morgan <daamorgan@hotmail.com>
2020-04-14 20:17:29 -0500

    changes to database url

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ UPLOAD_FOLDER='./app/static/uploads'
 app=Flask(__name__)
 app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
 app.config['SECRET_KEY']="Xz9am$hu7gry"
-app.config['SQLALCHEMY_DATABASE_URI']=DATABASE_URL='postgres://ljbxsvxojiuslh:18dc0343fac9a78f606ae0d46485550e3b74e71e6095dfea4e014a041c9086fa@ec2-3-211-48-92.compute-1.amazonaws.com:5432/dd4gj1qiq7naad' #"postgres://project1:password123@localhost/project1" 
+app.config['SQLALCHEMY_DATABASE_URI']=DATABASE_URL='postgresql://ljbxsvxojiuslh:18dc0343fac9a78f606ae0d46485550e3b74e71e6095dfea4e014a041c9086fa@ec2-3-211-48-92.compute-1.amazonaws.com:5432/dd4gj1qiq7naad' #"postgres://project1:password123@localhost/project1" 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
 
